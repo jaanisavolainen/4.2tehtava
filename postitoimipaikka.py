67a8e88ea8f31cb94c9346e0e96aa3171f8b62c7
Jaani Savolainen <jaani.savolainen@gmail.com>
2020-10-06 11:18:36 +0300

    vittu

--- postitoimipaikka.py
+++ postitoimipaikka.py
@@ -1,34 +1,24 @@
+import urllib.request
 import json
-import os, sys
-import array
-# JSON-data on tässä kovakoodattu moniriviseksi merkkijonoksi.
-# Käytä `open`-funktiota lukeaksesi tiedot levyltä tai
-# `urllib`-kirjastoa lukeaksesi tiedot suoraan netistä.
 
-# print(os.path.join(sys.path[0],"postcode_map.json"))
-#print (pathi)
+JSON_URL = 'https://raw.githubusercontent.com/theikkila/postinumerot/master/postcode_map_light.json'
 
-def luo_data_postitoimipaikat():
-  pathi = os.path.join(sys.path[0],"postcode_map_light.json")
-  with open(pathi) as f:
-    data = json.load(f)
 
-  newDict = {
+def hae_postinumerot():
+    with urllib.request.urlopen(JSON_URL) as response:
+        return json.loads(response.read())
 
-  }
-  for postinumero, postitoimipaikka in data.items():
-    if not postitoimipaikka in newDict:
-      newDict[postitoimipaikka] = [postinumero]
+
+def main():
+    haettava = input('Kirjoita postinumero: ').strip()
+
+    postinumerot = hae_postinumerot()
+
+    if haettava in postinumerot:
+        print(postinumerot[haettava].title())
     else:
-      temparray = newDict[postitoimipaikka]
-      temparray.append(postinumero)
-      newDict[postitoimipaikka] = temparray
-  return newDict
+        print('Postitoimipaikkaa ei löytynyt :(')
 
 
-def etsi_postitoimipaikka(postitoimipaikka,data):
-  results = []
-  if postitoimipaikka.upper() in data:
-    for nro in data[postitoimipaikka.upper()]:
-      results.append(nro)
-  return results
+if __name__ == '__main__':
+    main()
